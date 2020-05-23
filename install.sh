04d9a8d61c1e066bab26f5d0c872101f478bffbe
Sarada1101 <ryoyukikazu11@gmail.com>
2020-05-23 16:50:58 +0900

    add

---
+++ install.sh
@@ -0,0 +1,18 @@
+# 今のディレクトリ
+# dotfilesディレクトリに移動する
+cd ~/dotfiles
+
+echo "start setup..."
+
+# dotfilesディレクトリにある、ドットから始まり2文字以上の名前のファイルに対して
+for f in .??*; do
+    [ "$f" = ".git" ] && continue
+    [ "$f" = ".gitconfig.local.template" ] && continue
+    [ "$f" = ".require_oh-my-zsh" ] && continue
+    [ "$f" = ".gitmodules" ] && continue
+
+    # シンボリックリンクを貼る
+    ln -snfv ${PWD}/"$f" ~/
+done
+
+echo "end"
