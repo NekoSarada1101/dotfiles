53263be900260069043fc6c090bee6b9b044aeef
Sarada1101 <ryoyukikazu11@gmail.com>
2020-06-10 13:16:21 +0900

    udpate

--- bin/install.sh
+++ bin/install.sh
@@ -19,7 +19,7 @@ done
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
 setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N) -a  "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^zshrc(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
